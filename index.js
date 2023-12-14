d9bd1a81eeb06ae5cd262edfe4b1a9d85965aab7
alexandrapesantez <103144953+alexandrapesantez@users.noreply.github.com>
2023-12-14 16:07:06 -0700

    ad SL

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ const knex = require("knex")({
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DB_NAME,
     port: process.env.RDS_PORT,
+    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
   },
 });
 
